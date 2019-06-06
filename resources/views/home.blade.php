b0122877c30a1397a4fef75b1f8b9ced6f21727e
Ayush Poudel <ayush_pod@gmail.com>
2019-06-06 22:19:19 +0000

    Rating

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,7 +20,16 @@
 					<div class="card mb-4 shadow-sm">
 						<img src="<?= asset("storage/profile/$user->profile_picture") ?>" width="100%" height="225" class="card-img-top"/>
 						<div class="card-body">
-							<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+							<p>
+							<a class="float-left" href="#"><strong>{{$user->name}}</strong></a>
+								<span class="float-right"><i class="text-warning fa fa-star"></i></span>
+								<span class="float-right"><i class="text-warning fa fa-star"></i></span>
+								<span class="float-right"><i class="text-warning fa fa-star"></i></span>
+								<span class="float-right"><i class="text-warning fa fa-star"></i></span>
+
+        	      			 </p>
+							<p class="card-text"></p>
+							<p class="card-text">{{$user->skills}}</p>
 							<div class="d-flex justify-content-between align-items-center">
 								<div class="btn-group">
 									<a href="{{route('user.detail', $user->id)}}" class="btn btn-sm btn-outline-secondary">View</a>
